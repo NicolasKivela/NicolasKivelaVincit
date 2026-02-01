a24e78d3579246ca0cf824eb758d28bb1c5f5360
Nicolas Kivelä <nicolas.kivela@gmail.com>
2026-02-01 15:48:52 +0200

    Apufunktio siirretty utils.py tiedostoon

--- database/database.py
+++ database/database.py
@@ -1,3 +1,7 @@
+from typing import List 
 from schemas.reservation import Reservation
-# In-memory "database"
+
+
+# Temporary In-memory "database"
+# TODO: Add database connection and queries here
 db_reservations: List[Reservation] = []
---
+++ utils.py
@@ -0,0 +1,11 @@
+from schemas.reservation import Reservation
+from database.database import db_reservations
+# --- Helper Functions ---
+def check_overlap(new_res: Reservation) -> bool:
+    """Checks if the new reservation overlaps with existing ones."""
+    for res in db_reservations:
+        if res.room_id == new_res.room_id:
+            # Logic: (StartA < EndB) AND (StartB < EndA)
+            if new_res.start_time < res.end_time and res.start_time < new_res.end_time:
+                return True
+    return False
